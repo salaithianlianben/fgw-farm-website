d0e4209e82b999f27b22bc2142c038b383b36fc4
Thian Lian Ben <thianlianben@coosy.co.jp>
2025-07-09 10:51:37 +0700

    update product error

--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -4,13 +4,13 @@ import { Product } from "@/types/product";
 import SectionTitle from "../Common/SectionTitle";
 import ProductItem from "./ProductItem";
 import { useTranslation } from "@/hooks/useTranslation";
-import { en_products_data } from "@/app/products/data/products";
+import { en_products_data, vn_products_data } from "@/app/products/data/products";
 import { vn_news_data } from "@/app/news/data";
 
 const ProductsFeature = () => {
   const { t, currentLanguage } = useTranslation();
-  const data = currentLanguage === "en" ? en_products_data : vn_news_data;
-  
+  const data = currentLanguage === "en" ? en_products_data : vn_products_data;
+
     return <>
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
