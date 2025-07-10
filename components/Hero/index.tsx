08f6c9ad02cc40686f2c739ef101e5bd7637ad29
Thian Lian Ben <thianlianben@coosy.co.jp>
2025-07-10 08:34:50 +0700

    update

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -11,27 +11,25 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="relative z-10 overflow-hidden bg-cover bg-center pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        className="relative z-10 overflow-hidden bg-cover bg-center pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
         style={{ backgroundImage: `url('${images_set[0]}')` }}
       >
         {/* Dark overlay */}
-        <div className={`absolute inset-0 z-0 bg-opacity-20`}></div>
+        <div className={`bg-opacity-20 absolute inset-0 z-0`}></div>
 
         {/* Content */}
-        <div className="container relative z-10">
+        <div className="relative z-10 container">
           <div className="-mx-4 flex flex-wrap">
             <div className="flex w-full flex-row justify-end px-4">
               <div
                 className="wow fadeInUp max-w-[800px] text-center"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  전염병의 창궐로 매우 심각한 위기에 놓여 있습니다
+                <h1 className="mb-5 text-3xl leading-tight font-bold text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  블랙 솔저 플라이 사육으로 지구를 살리고, 자원을 순환시킵니다.
                 </h1>
                 <p className="mb-12 text-base leading-relaxed! text-gray-200 sm:text-lg md:text-xl">
-                  인류가 함께 숨 쉬고 살고 있는 지구환경은 현재 기후 온난화로
-                  인한 각종 환경 재해, 식량의 분배 및 처리 문제와 수많은
-                  전염병의 창궐로 매우 심각한 위기에 놓여 있습니다.
+                  “지속 가능한 미래, 곤충에서 시작됩니다.”
                 </p>
               </div>
             </div>
