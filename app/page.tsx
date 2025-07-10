468b538d5a2a54acdc7e8f8cf842e5f135aba4b5
Thian Lian Ben <thianlianben@coosy.co.jp>
2025-07-10 17:03:01 +0700

    udpate header

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "FGW FARM",
-  description: "This is Home for Startup Nextjs Template",
+  description: "FGW FARM CO., LTD",
 };
 
 export default function Home() {
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Sign In Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Sign In Page for Startup Nextjs Template",
+  description: "FGW FARM CO., LTD",
   // other metadata
 };
 
